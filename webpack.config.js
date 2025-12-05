16329c880ee37d91bab9acce04d0194053ca7158
Vadim Obanin <mortido3@mail.ru>
2025-12-05 11:39:34 +0300

    feat: настроить базовую сборку с Webpack
    - добавить webpack, webpack-cli и html-webpack-plugin как dev-зависимости
    - создать конфигурацию webpack для сборки точек входа и генерации HTML
    - настроить выходную директорию build с очисткой и хешированием имён файлов
    - добавить шаблон index.html в папку public с точкой монтирования #root

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
+
+module.exports = {
+  mode: 'production',
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'build'),
+    clean: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html')
+    }),
+    new webpack.ProgressPlugin()
+  ],
+}
