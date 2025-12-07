f789238e96a1e23d531ec5fcaa639b535ed1dfe3
Vadim Obanin <mortido3@mail.ru>
2025-12-07 17:58:37 +0300

    feat: добавить роутинг и асинхронную загрузку страниц
    - в webpack.config.ts добавить флаг для поддержки корректного обновления страниц вне корневого роута
    - добавить тестовые компоненты страниц и реализовать асинхронную подгрузку с помощью lazy и Suspence
    - разбить бандл на чанки соответствующие страницам

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import {Counter} from "./components/Counter";
 import './index.scss';
+import {Link, Route, Routes} from "react-router-dom";
+
+import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
+import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+import {Suspense} from "react";
 
 export const App = () => {
-    return (
-        <>
-            <h1 className={'app'}>App Component</h1>
-            <Counter/>
-        </>
-    )
+  return (
+    <>
+      <Link to={'/'}>Main</Link>
+      <Link to={'/about'}>About</Link>
+      <Suspense fallback={'Loading...'}>
+        <Routes>
+          <Route path={'/about'} element={<AboutPageAsync/>}/>
+          <Route path={'/'} element={<MainPageAsync/>}/>
+        </Routes>
+      </Suspense>
+      <h1 className={'app'}>App Component</h1>
+      <Counter/>
+    </>
+  )
 }
