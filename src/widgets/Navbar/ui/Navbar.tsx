119a4f2343b2866c052d9ec6a2665cb8d28ba11b
Vadim Obanin <mortido3@mail.ru>
2025-12-16 15:51:21 +0300

    feat: добавить модальное окно

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, {useCallback, useState} from 'react';
 import {classNames} from "shared/lib/classNames/classNames";
 import cls from './Navbar.module.scss';
+import {Modal} from "shared/ui/Modal/Modal";
+import {useTranslation} from "react-i18next";
+import {Button} from "shared/ui/Button";
+import {ThemeButton} from "shared/ui/Button/ui/Button";
 
 interface NavbarProps {
   className?: string;
 }
 
 export const Navbar = ({className}: NavbarProps) => {
+  const {t} = useTranslation();
+  const [isAuthModal, setIsAuthModal] = useState(false);
+
+  const onToggleModal = useCallback(() => {
+    setIsAuthModal(prev => !prev);
+  }, []);
+
   return (
     <div className={classNames(cls.navbar, {}, [className])}>
-      <div className={cls.links}>
-        /
-      </div>
+      <Button theme={ThemeButton.CLEAR} className={cls.links} onClick={onToggleModal}>
+        {t('Войти')}
+      </Button>
+      <Modal isOpen={isAuthModal} onClose={onToggleModal}>
+        Conternt sdfs dfs dfs dfsdf
+      </Modal>
     </div>
   );
 };
